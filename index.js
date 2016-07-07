dc51ff74bb5a3aa1c46fe2824467862f5889b0b5
jspark311 <josh.lindsay@gmail.com>
2016-07-07 11:51:26 -0700

    Leaving myself some final notes and stubs.

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 'use strict';
 
+// NOTE: Because this is not automatically derived from the library's compiled-in
+//         support, care must be taken to ensure that the desired ciphersuite is
+//         *actually* supported, or else you will chase your tail.
+//       The idea here is to make an enum that can be chosen from JS and have the
+//         choice reflected in the allowed_ciphersuites array in the native code.
+const supported_ciphersuites = Object.freeze({
+  TLS_PSK_WITH_AES_128_CCM_8:                0xC0A8,
+  TLS_ECDHE_PSK_WITH_AES_128_CBC_SHA256:     0xC037,
+  TLS_PSK_WITH_AES_128_CBC_SHA256:           0xAE
+});
+
 var DtlsServer = require('./server');
 const DtlsClientSocket = require('./client_socket');
 
